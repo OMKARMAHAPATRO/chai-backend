840aa11b7b5db9d3b0f621e748e105791f9707e5
Omkar Mahapatro <mahapatroomkar@gmail.com>
2025-07-20 13:59:04 +0530

    comment

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -14,7 +14,7 @@ const asyncHandler =  (requestHandler) =>{
 
 // const asyncHandler = () =>{}
 // const asyncHandler = (func) =>{ () => {} }
-// // this function is a asynchronic function
+// // this function is a asynchronic  higher order function - Where we pass a function an argument / pass a finction as an paramaeter 
 //  const asyncHandler = (func) => async() => {}
 // // higher order function
 // const asyncHandler = (fn) => () => {}
@@ -39,3 +39,37 @@ const asyncHandler =  (requestHandler) =>{
 //         })
 //     }
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Note :-
+
+/*
+
+
+const asyncHandler =  (requestHandler) =>{
+    return (req, res , next) =>{
+        Promise.resolve(requestHandler(req, res, next)). _> if the promise resolve then it will return the requestHandler(req, res, next). if the promise is rejected then it will go to the catch block.
+        catch((err) => next(err))
+    }
+}
+
+
+// promise syntax : - 
+              let myPromise = new Promise(function(resolve, catch)
+
+*/
+   
+// in this file we  make a warapper function under the name of asyncHandler which will take a requestHandler as an argument and return a function that takes req, res, and next as arguments. 
+// Inside this function, we use Promise.resolve to execute the requestHandler and catch any errors that occur, passing them to the next middleware.
